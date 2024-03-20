213c3d6608feae7b6b45bdc5042d93e38f13647f
VS1379 <valentin.silva.user@gmail.com>
2024-03-20 18:40:16 -0300

    finish product and product detail

--- src/components/ProductDetail.jsx
+++ src/components/ProductDetail.jsx
@@ -12,7 +12,8 @@ function ProductDetail() {
     console.log("Montado");
     const fetchdata = async () => {
       try {
-        SetProduct(await ProductsAPI.getById(id.match));
+        let data = await ProductsAPI.getById(id.id);
+        SetProduct(data.data);
       } catch (error) {
         console.error("Error al obtener el producto:", error);
       }
@@ -21,30 +22,35 @@ function ProductDetail() {
   }, []);
   useEffect(() => {
     console.log("Actualizado");
-    console.log(product);
   }, [product]);
+
+  function imagen() {
+    if (product.image) {
+      product.image = product.image.slice(product.image.indexOf("https"), [
+        -2 + product.image.length,
+      ]);
+      return product.image;
+    }
+  }
 
   return (
     <div className="col-lg-6 mb-4">
       <div className="card shadow mb-4">
         <div className="card-header py-3">
-          <h5 className="m-0 font-weight-bold text-gray-800">
-            {"TITULO"}
-            {"no id"}
-          </h5>
+          <h5 className="m-0 font-weight-bold text-gray-800">{product.name}</h5>
         </div>
         <div className="card-body">
           <div className="text-center">
             <img
               className="img-fluid px-3 px-sm-4 mt-3 mb-4"
               style={{ width: 40 + "rem" }}
-              src={"imagenFondo"}
-              alt=" Star Wars - Mandalorian "
+              alt="Barco"
+              src={imagen()}
             />
           </div>
-          <p>{"DESCRIPCION"}</p>
+          <p>{product.description}</p>
           <a className="btn btn-danger" target="_blank" rel="nofollow" href="/">
-            {"IR AL SITIO"}
+            {"URL"}
           </a>
         </div>
       </div>
